4d0639e3f402f079d1308d47a153143438f0b83d
AnARCHIS12 <fakachmpionship@gmail.com>
2025-08-22 16:29:09 +0200

    Développement local

--- dev-gui/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Script de lancement pour Windows
-
-cd /d "%~dp0"
-python dev_gui.py
-pause
--- dev-gui/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script de lancement pour Linux
-
-cd "$(dirname "$0")"
-export FONTCONFIG_FILE=/dev/null
-export DISPLAY=:0
-python3 dev_gui.py >/dev/null 2>&1 &
-echo "Interface graphique lancée en arrière-plan"
